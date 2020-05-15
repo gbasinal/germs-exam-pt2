b9059b3cf0cfcc2eb09cfdb75721a5a3fda40a19
gierMann <thisisgie.21@gmail.com>
2020-05-15 18:34:13 +0700

    UPDATE: added structure of hamburger menu in tablet view

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -4,9 +4,20 @@
     var Menu = function() {};
     
 	Menu.prototype.init = function() {
-	};
+        var _self = this;
+        _self.burgerClick();
+    };
+    
 
     app.Menu = Menu;
+
+
+    Menu.prototype.burgerClick = function(){
+        $(".hamburger").on("click", function(){
+            $(this).toggleClass("is-active");
+        })
+    };
+
 
     app.ready(function () {
         Menu.prototype.init()
