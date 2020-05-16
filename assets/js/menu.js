c84cbe4b366fc6680c0ab47ccd82a96009bd1954
gierMann <thisisgie.21@gmail.com>
2020-05-16 17:32:49 +0700

    UPDATE: added scrollspy functionality

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -7,6 +7,7 @@
         var _self = this;
         _self.burgerClick();
         _self.menuItemClick();
+        _self.setMenuActiveStateWhenScrolling();
     };
     
 
@@ -49,6 +50,77 @@
             $(this).addClass("active");
 
         })
+    }
+
+    Menu.prototype.setMenuActiveStateWhenScrolling = function (){
+
+
+        var is_colliding = function($div1, $div2) {
+            // Div 1 data
+            var d1_offset = $div1.offset();
+            var d1_height = $div1.outerHeight(true);
+            var d1_width = $div1.outerWidth(true);
+            var d1_distance_from_top = d1_offset.top + d1_height;
+            var d1_distance_from_left = d1_offset.left + d1_width;
+        
+            // Div 2 data
+            var d2_offset = $div2.offset();
+            var d2_height = $div2.outerHeight(true);
+            var d2_width = $div2.outerWidth(true);
+            var d2_distance_from_top = d2_offset.top + d2_height;
+            var d2_distance_from_left = d2_offset.left + d2_width;
+        
+            var not_colliding = (d1_distance_from_top < d2_offset.top || d1_offset.top > d2_distance_from_top || d1_distance_from_left < d2_offset.left || d1_offset.left > d2_distance_from_left);
+        
+            // Return whether it IS colliding
+            return !not_colliding;
+        };
+
+        $(window).on("scroll", function(){
+            var home =$("#home");
+            var momentum =$("#lorem-momentum");
+            var artist =$("#the-artist");
+            var artist2 =$("#the-artist-2");
+            var redeem =$("#redeem");
+  
+            var collider = $(".collide");
+
+            $(".nav-header-wrapper ul li a").removeClass("active")
+            if(is_colliding(home, collider)){
+                $("[href='#home']").addClass("active")
+            }
+            if(is_colliding(momentum, collider)){
+                $("[href='#lorem-momentum']").addClass("active")
+            }
+            if(is_colliding(artist, collider)){
+                $("[href='#the-artist']").addClass("active")
+            }
+            if(is_colliding(artist2, collider)){
+                $("[href='#the-artist']").addClass("active")
+            }
+            if(is_colliding(redeem, collider)){
+                $("[href='#redeem']").addClass("active")
+            }
+        })
+
+        // $(".section").on('inview', function(visible) {
+            
+        //     var _self = $(this);
+        //     var id = _self.attr("id");
+        //     console.log(id)
+     
+        //     // $(".nav-header-wrapper ul li a").removeClass("active")
+        //     // $("[href='#"+id+"']").addClass("active")
+
+
+        //     if (visible) {
+               
+        //         $(".nav-header-wrapper ul li a").removeClass("active")
+        //         $("[href='#"+id+"']").addClass("active")
+              
+        //     }
+
+        // });
     }
 
 
