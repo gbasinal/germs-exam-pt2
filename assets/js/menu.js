6391bc4517828682d0b4802409f4631b21ec1f92
gierMann <thisisgie.21@gmail.com>
2020-05-16 17:40:26 +0700

    UPDATE: added close functionality of menu when user clicks or taps a menu item

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -48,34 +48,41 @@
             $('body,html').animate({ scrollTop: position}, 500);
             item.removeClass("active");
             $(this).addClass("active");
-
+            if($(window).outerWidth() < 991){
+                setTimeout(function(){
+                    $(".hamburger").trigger("click");
+                },500)
+                
+            }
         })
     }
 
+    Menu.prototype.collider = function($div1, $div2){
+       
+        // Div 1 data
+        var d1_offset = $div1.offset();
+        var d1_height = $div1.outerHeight(true);
+        var d1_width = $div1.outerWidth(true);
+        var d1_distance_from_top = d1_offset.top + d1_height;
+        var d1_distance_from_left = d1_offset.left + d1_width;
+    
+        // Div 2 data
+        var d2_offset = $div2.offset();
+        var d2_height = $div2.outerHeight(true);
+        var d2_width = $div2.outerWidth(true);
+        var d2_distance_from_top = d2_offset.top + d2_height;
+        var d2_distance_from_left = d2_offset.left + d2_width;
+    
+        var not_colliding = (d1_distance_from_top < d2_offset.top || d1_offset.top > d2_distance_from_top || d1_distance_from_left < d2_offset.left || d1_offset.left > d2_distance_from_left);
+    
+        // Return whether it IS colliding
+        return !not_colliding;
+        
+
+    }
+
     Menu.prototype.setMenuActiveStateWhenScrolling = function (){
-
-
-        var is_colliding = function($div1, $div2) {
-            // Div 1 data
-            var d1_offset = $div1.offset();
-            var d1_height = $div1.outerHeight(true);
-            var d1_width = $div1.outerWidth(true);
-            var d1_distance_from_top = d1_offset.top + d1_height;
-            var d1_distance_from_left = d1_offset.left + d1_width;
-        
-            // Div 2 data
-            var d2_offset = $div2.offset();
-            var d2_height = $div2.outerHeight(true);
-            var d2_width = $div2.outerWidth(true);
-            var d2_distance_from_top = d2_offset.top + d2_height;
-            var d2_distance_from_left = d2_offset.left + d2_width;
-        
-            var not_colliding = (d1_distance_from_top < d2_offset.top || d1_offset.top > d2_distance_from_top || d1_distance_from_left < d2_offset.left || d1_offset.left > d2_distance_from_left);
-        
-            // Return whether it IS colliding
-            return !not_colliding;
-        };
-
+        // Code below can still be optimized
         $(window).on("scroll", function(){
             var home =$("#home");
             var momentum =$("#lorem-momentum");
@@ -86,41 +93,23 @@
             var collider = $(".collide");
 
             $(".nav-header-wrapper ul li a").removeClass("active")
-            if(is_colliding(home, collider)){
+            if(app.Menu.prototype.collider(home, collider)){
                 $("[href='#home']").addClass("active")
             }
-            if(is_colliding(momentum, collider)){
+            if(app.Menu.prototype.collider(momentum, collider)){
                 $("[href='#lorem-momentum']").addClass("active")
             }
-            if(is_colliding(artist, collider)){
+            if(app.Menu.prototype.collider(artist, collider)){
                 $("[href='#the-artist']").addClass("active")
             }
-            if(is_colliding(artist2, collider)){
+            if(app.Menu.prototype.collider(artist2, collider)){
                 $("[href='#the-artist']").addClass("active")
             }
-            if(is_colliding(redeem, collider)){
+            if(app.Menu.prototype.collider(redeem, collider)){
                 $("[href='#redeem']").addClass("active")
             }
         })
 
-        // $(".section").on('inview', function(visible) {
-            
-        //     var _self = $(this);
-        //     var id = _self.attr("id");
-        //     console.log(id)
-     
-        //     // $(".nav-header-wrapper ul li a").removeClass("active")
-        //     // $("[href='#"+id+"']").addClass("active")
-
-
-        //     if (visible) {
-               
-        //         $(".nav-header-wrapper ul li a").removeClass("active")
-        //         $("[href='#"+id+"']").addClass("active")
-              
-        //     }
-
-        // });
     }
 
 
